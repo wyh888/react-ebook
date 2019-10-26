dc8e602d807d66efeea14ac0b507c23b4b32dd79
wangyh <yuho_wang@163.com>
2019-10-26 10:21:14 +0800

    标题栏菜单栏实现

--- src/pages/Ebook/index.js
+++ src/pages/Ebook/index.js
@@ -1,19 +1,77 @@
 import React, { Component } from 'react'
 import './index.scss'
 import Epub from 'epubjs'
+import { CSSTransition } from 'react-transition-group'
 
 const DOWNLOAD_URL = '/ebook/2018_Book_AgileProcessesInSoftwareEngine.epub'
 
 export default class Ebook extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      ifTitleAndMenuShow: false
+    }
+    this.prevPage = this.prevPage.bind(this)
+    this.nextPage = this.nextPage.bind(this)
+    this.toggleTitleAndMenu = this.toggleTitleAndMenu.bind(this)
+  }
+
   render () {
     return (
-      <div className="ebook">
-        <div className="read-wrapper">
-          <div id="read"></div>
-          <div className="mask">
-            <div className="left" onClick={this.prevPage.bind(this)}></div>
-            <div className="center"></div>
-            <div className="right" onClick={this.nextPage.bind(this)}></div>
+      <div className='ebook'>
+        <CSSTransition
+          in={this.state.ifTitleAndMenuShow}
+          timeout={300}
+          classNames='slide-down'
+          unmountOnExit
+        >
+          <div className='title-wrapper'>
+            <div className='left'>
+              <span className='icon icon-back'></span>
+            </div>
+            <div className='right'>
+              <div className='icon-wrapper'>
+                <span className='icon icon-cart'></span>
+              </div>
+              <div className='icon-wrapper'>
+                <span className='icon icon-person'></span>
+              </div>
+              <div className='icon-wrapper'>
+                <span className='icon icon-more'></span>
+              </div>
+            </div>
+          </div>
+        </CSSTransition>
+
+        <CSSTransition
+          in={this.state.ifTitleAndMenuShow}
+          timeout={300}
+          classNames="slide-up"
+          unmountOnExit
+        >
+          <div className='menu-wrapper'>
+            <div className='icon-wrapper'>
+              <span className='icon icon-menu'></span>
+            </div>
+            <div className='icon-wrapper'>
+              <span className='icon icon-progress'></span>
+            </div>
+            <div className='icon-wrapper'>
+              <span className='icon icon-bright'></span>
+            </div>
+            <div className='icon-wrapper'>
+              <span className='icon icon-a'>A</span>
+            </div>
+          </div>
+        </CSSTransition>
+
+        <div className='read-wrapper'>
+          <div id='read'></div>
+          <div className='mask'>
+            <div className='left' onClick={this.prevPage}></div>
+            <div className='center' onClick={this.toggleTitleAndMenu}></div>
+            <div className='right' onClick={this.nextPage}></div>
           </div>
         </div>
       </div>
@@ -24,16 +82,24 @@ export default class Ebook extends Component {
     this.showEpub()
   }
 
+  toggleTitleAndMenu () {
+    this.setState({
+      ifTitleAndMenuShow: !this.state.ifTitleAndMenuShow
+    })
+  }
+
   prevPage () {
-    if (this.rendition) {
-      this.rendition.prev()
-    }
+    this.rendition && this.rendition.prev()
+    this.state.ifTitleAndMenuShow && this.setState({
+      ifTitleAndMenuShow: false
+    })
   }
 
   nextPage () {
-    if (this.rendition) {
-      this.rendition.next()
-    }
+    this.rendition && this.rendition.next()
+    this.state.ifTitleAndMenuShow && this.setState({
+      ifTitleAndMenuShow: false
+    })
   }
 
   showEpub () {
